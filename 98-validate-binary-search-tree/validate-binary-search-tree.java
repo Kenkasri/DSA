7f3bfc14b838701e8a5206f77bd1cf885a27234c
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-03-23 14:29:01 +0530

    Time: 2 ms (12.37%) | Memory: 45.4 MB (29.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,12 +14,29 @@
  * }
  */
 class Solution {
-  public boolean isValid(TreeNode root,long min,long max){
-    if(root==null) return true;;
-    if(root.val<=min||root.val>=max) return false;
-    return isValid(root.left,min,root.val)&&isValid(root.right,root.val,max);
+  // public boolean isValid(TreeNode root,long min,long max){
+  //   if(root==null) return true;;
+  //   if(root.val<=min||root.val>=max) return false;
+  //   return isValid(root.left,min,root.val)&&isValid(root.right,root.val,max);
+  // }
+  public TreeNode inorder(TreeNode root,  ArrayList<Integer>list){
+    if(root==null){
+      return null;
+    }
+    inorder(root.left,list);
+    list.add(root.val);
+    inorder(root.right,list);
+    return root;
   }
     public boolean isValidBST(TreeNode root) {
-        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        // return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+       ArrayList<Integer>list = new ArrayList<>();
+       inorder(root,list);
+       for(int i = 1;i<list.size();i++){
+        if(list.get(i-1)>=list.get(i)){
+           return false;
+        }
+       }
+       return true;
     }
 }
