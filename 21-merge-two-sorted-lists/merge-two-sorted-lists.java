e08282bb5548ab490cd0120013553b62a94d6ce1
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-07-29 10:13:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (88.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,35 +10,37 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null){
-          return list2;
+     if(list1==null){
+      return list2;
+     }
+     if(list2==null){
+      return list1;
+     }
+      ListNode temp1 = list1;
+      ListNode temp2 = list2;
+      ListNode nn = new ListNode(-1);
+      ListNode temp3 = nn;
+      while(temp1!=null&&temp2!=null){
+        if(temp1.val<temp2.val){
+          temp3.next = temp1;
+          temp1 = temp1.next;
+          temp3 = temp3.next;
+        }else{
+          temp3.next = temp2;
+          temp2 = temp2.next;
+          temp3 = temp3.next;
         }
-        if(list2==null){
-          return list1;
-        }
-        ListNode t1 = list1;
-        ListNode t2 = list2;
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
-        while(t1!=null&&t2!=null){
-           if(t1.val<t2.val){
-              cur.next = t1;
-              t1 = t1.next;
-             
-           }else{
-            cur.next  = t2;
-            
-            t2 = t2.next;
-           }
-            cur = cur.next;
-        }
-        if(t1!=null){
-          cur.next = t1;
+      }
+      if(temp1!=null){
+        temp3.next = temp1;
 
-        }
-        if(t2!=null){
-          cur.next = t2;
-        }
-        return dummy.next;
+      }
+      if(temp2!=null){
+        temp3.next = temp2;
+
+      }
+
+       return nn.next;
+       
     }
 }
