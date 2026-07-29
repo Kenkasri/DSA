4267b58f46e4a02f7a54d57b9eea429254196011
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-07-29 15:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (16.89%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -15,17 +15,14 @@ class Solution {
         }
         ListNode odd = head;
         ListNode even = head.next;
-        ListNode evenhead = head.next;
+        ListNode evenhead = even;
         while(even!=null&&even.next!=null){
           odd.next = even.next;
           odd = odd.next;
           even.next = odd.next;
           even = even.next;
-          
-          
         }
-        odd.next = evenhead;
-        return head;
-
+       odd.next = evenhead;
+       return head;
     }
 }
