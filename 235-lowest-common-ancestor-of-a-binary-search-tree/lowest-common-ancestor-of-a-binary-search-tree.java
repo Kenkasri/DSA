153676066f818d5d36cac6edbcd66280eafd1870
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-08-11 15:30:06 +0530

    Time: 6 ms (96.92%) | Memory: 47.9 MB (16.76%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,62 +9,17 @@
  */
 
 class Solution {
-    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    //     if(root==null){
-    //       return root;
-    //     }
-    //     if(p.val<root.val&&q.val<root.val){
-    //       return lowestCommonAncestor(root.left,p,q);
-    //     }
-    //     if(p.val>root.val&&q.val>root.val){
-    //       return lowestCommonAncestor(root.right,p,q);
-    //     }
-
-    //     return root;
-    // }
-        public TreeNode lowestCommonAncestor(TreeNode root,
-                                         TreeNode p,
-                                         TreeNode q) {
-
-        List<TreeNode> pathP = new ArrayList<>();
-        List<TreeNode> pathQ = new ArrayList<>();
-
-        findPath(root, p, pathP);
-        findPath(root, q, pathQ);
-
-        int i = 0;
-
-        while (i < pathP.size() &&
-               i < pathQ.size() &&
-               pathP.get(i) == pathQ.get(i)) {
-            i++;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null){
+          return null;
         }
 
-        return pathP.get(i - 1);
-    }
-
-    private boolean findPath(TreeNode root,
-                             TreeNode target,
-                             List<TreeNode> path) {
-
-        if (root == null) {
-            return false;
+        if(p.val<root.val&&q.val<root.val){
+          return lowestCommonAncestor(root.left,p,q);
         }
-
-        path.add(root);
-
-        if (root == target) {
-            return true;
+        if(p.val>root.val&&q.val>root.val){
+          return lowestCommonAncestor(root.right,p,q);
         }
-
-        if (findPath(root.left, target, path) ||
-            findPath(root.right, target, path)) {
-            return true;
-        }
-
-        // Backtrack
-        path.remove(path.size() - 1);
-
-        return false;
+        return root;
     }
 }
