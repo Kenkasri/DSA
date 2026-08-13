f8e4bdf0eb756cdceccba01509453e87f2cf3eed
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-08-13 11:57:34 +0530

    Time: 1 ms (79.91%) | Memory: 43.4 MB (91.37%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,33 +14,36 @@
  * }
  */
 class Solution {
-  
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        boolean flag = false;
-        List<List<Integer>>result = new ArrayList<>();
-        if(root==null){
-          return result;
+      boolean flag = false;
+     
+        List<List<Integer>>list = new ArrayList<>();
+        if(root == null){
+      return list;
+     }
+     Queue<TreeNode>q = new LinkedList<>();
+     q.add(root);
+     while(!q.isEmpty()){
+      int s = q.size();
+      List<Integer>l = new ArrayList<>();
+      for(int i = 0;i<s;i++){
+        TreeNode temp = q.poll();
+        if(temp.left!=null){
+               q.add(temp.left);
         }
-        Queue<TreeNode>q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-          List<Integer>list = new ArrayList<>();
-          int size = q.size();
-          for(int i = 0;i<size;i++){
-            TreeNode n = q.poll();
-            list.add(n.val);
-            if(n.left!=null) 
-              q.add(n.left);
-            if(n.right!=null) 
-              q.add(n.right);
-          }
-          if(flag==true){
-            Collections.reverse(list);
+         if(temp.right!=null){
+               q.add(temp.right);
+        }
+        l.add(temp.val);
 
-          }
-          result.add(new ArrayList<>(list));
-          flag = !flag;
+      }
+
+        if(flag==true){
+          Collections.reverse(l);
         }
-        return result;
+        list.add(new ArrayList<>(l));
+        flag = !flag;
+     }   
+     return list;
     }
 }
