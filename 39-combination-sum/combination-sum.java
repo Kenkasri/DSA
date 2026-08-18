258df31ae80301f84df3f38cae6edd46f587e37c
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-08-18 14:59:00 +0530

    Time: 2 ms (96.04%) | Memory: 45.8 MB (51.93%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,23 @@
 class Solution {
-  public void function(int[]candidates,int target,List<List<Integer>>result,List<Integer>com,int i){
-    if(target==0){
-      result.add(new ArrayList<>(com));
-      return;
-    }
-    if(target<0){
-      return;
-    }
-    for(int index = i;index<candidates.length;index++){
-       com.add(candidates[index]);
-       function(candidates,target-candidates[index],result,com,index);
-       com.remove(com.size()-1);
-    }
-  }
+      void sum(List<List<Integer>>result,List<Integer>cur,int[]arr,int target,int i){
+         if(target==0){
+          result.add(new ArrayList<>(cur));
+          return;
+         }
+         if(target<0){
+          return;
+         }
+         for(int index = i;index<arr.length;index++){
+                  cur.add(arr[index]);
+                  sum(result,cur,arr,target-arr[index],index);
+                  cur.remove(cur.size()-1);
+         }
+      }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<List<Integer>>result = new ArrayList<>();
-       List<Integer>com = new ArrayList<>();
-       function(candidates,target,result,com,0); 
-       return result;
+           List<List<Integer>>result = new ArrayList<>();
+           List<Integer>cur = new ArrayList<>();
+           sum(result,cur,candidates,target,0);
+           return result;
+
     }
 }
