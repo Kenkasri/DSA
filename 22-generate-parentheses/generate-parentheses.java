fac3fdcc212240885a2c9de2ccff47639f5ef521
Kenkasri <165180481+Kenkasri@users.noreply.github.com>
2026-08-18 10:37:47 +0530

    Time: 2 ms (70.01%) | Memory: 44.8 MB (44.67%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,19 +1,21 @@
 class Solution {
-  public void paraenthesis(List<String>list,int n,String s,int op,int cp){
+  public void parenthesis(int n,List<String>list,int op,int cp,String s){
     if(s.length()==n*2){
       list.add(s);
       return;
     }
     if(op<n){
-       paraenthesis(list,n,s+'(',op+1,cp);
+      parenthesis(n,list,op+1,cp,s+'(');
+
     }
     if(cp<op){
-      paraenthesis(list,n,s+')',op,cp+1);
+      parenthesis(n,list,op,cp+1,s+')');
     }
   }
     public List<String> generateParenthesis(int n) {
-       List<String>list =  new ArrayList<>();
-       paraenthesis(list,n,"",0,0);
-       return list; 
+     List<String>list = new ArrayList<>();
+     parenthesis(n,list,0,0,"");
+     return list;
+
     }
 }
